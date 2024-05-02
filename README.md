ec04e08b019b8ffdd5e559cbde6136ff8018ce23
LordNinth <rr621801@gmail.com>
2024-05-02 04:23:11 +0100

    multer

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # imgaeUpload
+
+multer is a node.js middleware for handling multipart/form-data which is primarily used for uploading files. It adds a body object and a file or files object to the request object. The body object contains the values of the text fields of the form, the file or files obj contains the files uploaded via the form.
+
+--make form enctype  ="multipart/form-data" in your form.
+
+
+upload.single for single files
+upload.array + multiple class added in form input for multiple files
--- app.js
+++ app.js
@@ -3,34 +3,34 @@ const app = express();
 const path = require("path");
 const ejsMate = require("ejs-mate");
 const multer = require("multer");
-
-
+const upload = multer({ dest: "uploads/" });
 
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({extended:true}))
-app.set("view engine", "ejs")
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
+const imgArray = [];
 
-const imgArray = [ ];
+app.get("/home", (req, res, next) => {
+  const image = imgArray;
+  res.render("home", { image });
+});
 
-app.get("/home", (req,res,next)=>{
-    const image = imgArray;
-    console.log(image)
-    res.render("home", {image})
-})
+app.post("/home", upload.array("avatar"), (req, res) => {
+  const { image } = req.body;
+  const {fieldname} = req.files;
+  console.log(req.files)
+//   imgArray.push(image, avatar);
+//   console.log(imgArray)
+  res.redirect("/home");
+});
+app.get("/new", (req, res) => {
+  res.render("new");
+});
 
-app.post("/home", (req,res)=>{
-    const {image} = req.body;
-    imgArray.push(image)
-    res.redirect("/home")
-})
-app.get("/new", (req,res)=>{
-    res.render("new")
-})
-
-app.use((err,req,res,next)=>{
-    console.log(err.message)
-})
-app.listen(3000, (req,res)=>{
-    console.log("listening 3000")
-})
+app.use((err, req, res, next) => {
+  console.log(err.message);
+});
+app.listen(3000, (req, res) => {
+  console.log("listening 3000");
+});
